5f36e80d0ca7c6156ab35554a1e0403b63d118b3
Sauloxd <saulotoshi@gmail.com>
2023-07-18 17:44:10 +0000

    Add everything required to start working

--- config/app.rb
+++ config/app.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 require "hanami"
+require "pry"
+require "redis"
+require_relative "./middlewares/rate_limit"
 
 module App
   class App < Hanami::App
+    self.redis = Redis.new(host: 'redis', port: 6379)
+    
+    config.middleware.use ::Middlewares::RateLimit, redis
   end
 end
