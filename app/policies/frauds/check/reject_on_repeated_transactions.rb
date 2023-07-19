69feee2096774241732c6c2b71180ef591870ac9
Sauloxd <saulotoshi@gmail.com>
2023-07-19 18:14:20 +0000

    :style:

--- app/policies/frauds/check/reject_on_repeated_transactions.rb
+++ app/policies/frauds/check/reject_on_repeated_transactions.rb
@@ -10,14 +10,15 @@ module App
           THRESHOLD_IN_SECONDS = 5 * 60 # 5 minutes
 
           def self.call(transaction)
-            last_transaction_at = ::App::Models::Transaction.last_user_transaction_at(user_id: transaction.user_id)
+            user_id = transaction.user_id
+            last_transaction_at = ::App::Models::Transaction.last_user_transaction_at(user_id: user_id)
             
             return if last_transaction_at.nil?
 
             seconds_since_last_transaction = Time.now.to_i - Time.parse(last_transaction_at).to_i
 
             if seconds_since_last_transaction < THRESHOLD_IN_SECONDS
-              "Invalid due to user #{transaction.user_id} transactioning too soon - last transaction was #{seconds_since_last_transaction} seconds ago."
+              "Invalid due to user #{user_id} transactioning too soon - last transaction was #{seconds_since_last_transaction} seconds ago."
             end
           end
         end
