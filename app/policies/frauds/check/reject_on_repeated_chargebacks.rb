36b951969190ccb54d14053eb92ebfc8b3fef6f8
Sauloxd <saulotoshi@gmail.com>
2023-07-19 17:11:54 +0000

    Reject if user already had a history of chargebacks

---
+++ app/policies/frauds/check/reject_on_repeated_chargebacks.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module App
+  module Policies
+    module Frauds
+      module Check 
+        # Objective from question: Reject transaction if a user had a chargeback before
+        # (note that this information does not comes on the payload. The chargeback data is received days after the transaction was approved)
+
+        class RejectOnRepeatedChargebacks
+          def self.call(request)
+            user_id = request.params[:user_id]
+            chargebacks = $REDIS.get("chargebacks:#{user_id}")
+            unless chargebacks.nil?
+              "Invalid due to repeated chargebacks. User #{user_id} already has #{chargebacks} chargebacks"
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
